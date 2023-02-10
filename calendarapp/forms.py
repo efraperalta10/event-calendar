d889f8912edb50eccec33ccd310a326c06d2950e
Efraín Peralta Rodríguez <56973952+efraperalta10@users.noreply.github.com>
2023-02-10 15:15:42 -0600

    arreglo icono y colores

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -10,12 +10,12 @@ class EventForm(ModelForm):
         # datetime-local is a HTML5 input type
         widgets = {
             "title": forms.TextInput(
-                attrs={"class": "form-control", "placeholder": "Enter event title"}
+                attrs={"class": "form-control", "placeholder": "Ingresar titúlo del evento"}
             ),
             "description": forms.Textarea(
                 attrs={
                     "class": "form-control",
-                    "placeholder": "Enter event description",
+                    "placeholder": "Ingresar descripción del evento",
                 }
             ),
             "start_time": DateInput(
